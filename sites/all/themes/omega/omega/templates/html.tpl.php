ad4c993e96d67cd327e28172c66a78e5cc5899de
Kevin Wiechmann <kevin.s.wiechmann@gmail.com>
2015-04-13 18:02:06 -0400

    Adding correct version of Omega theme

--- sites/all/themes/omega/omega/templates/html.tpl.php
+++ sites/all/themes/omega/omega/templates/html.tpl.php
@@ -2,10 +2,10 @@
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf->version . $rdf->namespaces; ?>>
 <head<?php print $rdf->profile; ?>>
   <?php print $head; ?>
-  <title><?php print $head_title; ?></title>  
+  <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
-  <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+  <!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
 <body<?php print $attributes;?>>
   <div id="skip-link">
